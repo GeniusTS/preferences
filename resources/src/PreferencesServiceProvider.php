e3e0ba89678a89f57e79a9d4319549b8cbbbf379
Abdul-Kader Zein-Eddin <aboudeh1987@gmail.com>
2016-09-08 01:06:29 +0300

    Add the service provider

---
+++ resources/src/PreferencesServiceProvider.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace GeniusTS\Preferences;
+
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class PreferencesServiceProvider
+ *
+ * @package GeniusTS\Core
+ */
+class PreferencesServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+//        $this->app->alias(PreferencesManager::class, 'Preferences');
+
+//        $this->app->singleton(PreferencesManager::class, function ()
+//        {
+//            return new PreferencesManager();
+//        });
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //        View::composer('geniusts_preferences::settings', function ($view)
+//        {
+//            return $view->with('$preferences', $this->app['preferences']);
+//        });
+    }
+}
