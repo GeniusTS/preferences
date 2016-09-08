65a428c45d646e15f1a8aab5ec0a30b29c7f756e
Abdul-Kader Zein-Eddin <aboudeh1987@gmail.com>
2016-09-09 02:59:44 +0300

    Add SettingsRequest

---
+++ src/Requests/SettingsRequest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace GeniusTS\Preferences\Requests;
+
+
+use GeniusTS\Preferences\Models\Domain;
+use Illuminate\Foundation\Http\FormRequest;
+
+/**
+ * Class SettingsRequest
+ *
+ * @package GeniusTS\Preferences
+ */
+class SettingsRequest extends FormRequest
+{
+
+    /**
+     * @var array
+     */
+    protected $rules = [];
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $preferences = resolve('preferences');
+
+        $preferences->domains
+            ->map(function (Domain $domain)
+            {
+                $this->rules = array_merge($this->rules, $domain->getRules());
+            });
+
+        return $this->rules;
+    }
+}
