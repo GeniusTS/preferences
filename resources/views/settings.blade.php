84b34e78353e2fdfa361064a82e9deb7c74cfe1c
Abdul-Kader Zein-Eddin <aboudeh1987@gmail.com>
2016-09-09 03:10:20 +0300

    Add the form tags to view

--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -1,5 +1,17 @@
-@if(version_compare($version, '5.3.0') < 0)
-    @include('geniusts_preferences::settings_5_2')
-@else
-    @include('geniusts_preferences::settings_5_3')
-@endif
+<form method="POST" action="{{ action('\GeniusTS\Preferences\Controllers\SettingsController@update') }}">
+    <input type="hidden" name="_method" value="PATCH" />
+
+    @if(version_compare($version, '5.3.0') < 0)
+        @include('geniusts_preferences::settings_5_2')
+    @else
+        @include('geniusts_preferences::settings_5_3')
+    @endif
+
+    <div class="row">
+        <div class="col-xs-12">
+            <button type="submit" class="btn btn-primary">
+                Save
+            </button>
+        </div>
+    </div>
+</form>
