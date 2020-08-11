03c08aff03af61dc7201d1bca311f1d3da99157e
Abdulkader Zeineddin <aboudeh1987@gmail.com>
2020-08-11 16:52:22 +0300

    Update SettingsControllers to allow saving specific domain or a specific element inside domain, and add request rules.

--- resources/controllers/SettingsController.php
+++ resources/controllers/SettingsController.php
@@ -22,19 +22,24 @@ class SettingsController extends PreferencesController
     /**
      * Display the edit form
      *
+     * @param \GeniusTS\Preferences\Models\Domain|null $domain
+     *
      * @return mixed
      */
-    public function edit()
+    public function edit($domain = null)
     {
-        return view('preferences.settings');
+        return view('preferences.settings', compact('domain'));
     }
 
     /**
      * Handle the success saving settings response
      *
+     * @param \GeniusTS\Preferences\Models\Domain|null  $domain
+     * @param \GeniusTS\Preferences\Models\Element|null $element
+     *
      * @return Response
      */
-    protected function handleSuccessResponse()
+    protected function handleSuccessResponse($domain, $element)
     {
         return redirect($this->redirectTo);
     }
--- src/Controllers/SettingsController.php
+++ src/Controllers/SettingsController.php
@@ -3,6 +3,7 @@
 namespace GeniusTS\Preferences\Controllers;
 
 
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\App;
@@ -14,7 +15,6 @@ use GeniusTS\Preferences\Models\Setting;
 use GeniusTS\Preferences\PreferencesManager;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Symfony\Component\HttpFoundation\Response;
-use GeniusTS\Preferences\Requests\SettingsRequest;
 use GeniusTS\Preferences\Events\PreferencesUpdated;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use GeniusTS\Preferences\Events\BeforeUpdatePreference;
@@ -53,21 +53,25 @@ abstract class SettingsController extends Controller
     /**
      * Handle update settings request
      *
-     * @param \GeniusTS\Preferences\Requests\SettingsRequest $request
+     * @param \Illuminate\Http\Request
+     * @param \GeniusTS\Preferences\Models\Domain|null  $domain
+     * @param \GeniusTS\Preferences\Models\Element|null $element
      *
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function update(SettingsRequest $request)
+    public function update(Request $request, $domain = null, $element = null)
     {
+        $this->validate($request, $this->rules($request));
+
         if ($this->transactions)
         {
-            DB::transaction(function () use ($request) {
-                $this->process($request);
+            DB::transaction(function () use ($request, $domain, $element) {
+                $this->process($request, $domain, $element);
             });
         }
         else
         {
-            $this->process($request);
+            $this->process($request, $domain, $element);
         }
 
         if (App::configurationIsCached())
@@ -75,27 +79,33 @@ abstract class SettingsController extends Controller
             Artisan::call('config:cache');
         }
 
-        return $this->handleSuccessResponse();
+        return $this->handleSuccessResponse($domain, $element);
     }
 
     /**
      * Saving settings process
      *
-     * @param \GeniusTS\Preferences\Requests\SettingsRequest $request
+     * @param \Illuminate\Http\Request                  $request
+     * @param \GeniusTS\Preferences\Models\Domain|null  $domain
+     * @param \GeniusTS\Preferences\Models\Element|null $element
      */
-    protected function process(SettingsRequest $request)
+    protected function process(Request $request, Domain $domain = null, Element $element = null)
     {
         Event::fire(new BeforeUpdatePreference);
 
-        /** @var Domain $domain */
-        foreach ($this->preferences->domains as $domain)
+        if ($element)
         {
-            /** @var Element $element */
-            foreach ($domain->elements as $element)
+            $this->saveElement($domain, $element, $request);
+        }
+        elseif ($domain)
+        {
+            $this->processDomainElements($domain, $request);
+        }
+        else
+        {
+            foreach ($this->preferences->domains as $namespace)
             {
-                $model = Setting::findBySlugOrNew($element->name, $domain->key);
-                $model->value = $request->get($element->name, null);
-                $model->save();
+                $this->processDomainElements($namespace, $request);
             }
         }
 
@@ -103,7 +113,101 @@ abstract class SettingsController extends Controller
     }
 
     /**
+     * Saving domain elements to database
+     *
+     * @param \GeniusTS\Preferences\Models\Domain $domain
+     * @param \Illuminate\Http\Request            $request
+     */
+    protected function processDomainElements(Domain $domain, Request $request)
+    {
+        /** @var Element $element */
+        foreach ($domain->elements as $element)
+        {
+            $this->saveElement($domain, $element, $request);
+        }
+    }
+
+    /**
+     * Save element to database
+     *
+     * @param \GeniusTS\Preferences\Models\Domain  $domain
+     * @param \GeniusTS\Preferences\Models\Element $element
+     * @param \Illuminate\Http\Request             $request
+     */
+    protected function saveElement(Domain $domain, Element $element, Request $request)
+    {
+        $model = Setting::findBySlugOrNew($element->name, $domain->key);
+        $model->value = $this->getElementValue($domain, $element, $request);
+        $model->save();
+    }
+
+    /**
+     * Get element value from request
+     *
+     * @param \GeniusTS\Preferences\Models\Domain  $domain
+     * @param \GeniusTS\Preferences\Models\Element $element
+     * @param \Illuminate\Http\Request             $request
+     *
+     * @return mixed
+     */
+    protected function getElementValue(Domain $domain, Element $element, Request $request)
+    {
+        return $request->input("{$domain->key}.{$element->name}", null);
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return array
+     */
+    protected function rules(Request $request)
+    {
+        /** @var \GeniusTS\Preferences\Models\Domain $domain */
+        $domain = $request->route()->parameter('preferences_domain');
+
+        if ($domain)
+        {
+            return $this->singleDomainRules($domain);
+        }
+
+        return $this->preferencesRules();
+    }
+
+    /**
+     * get rule for a specific domain
+     *
+     * @param \GeniusTS\Preferences\Models\Domain $domain
+     *
+     * @return array
+     */
+    protected function singleDomainRules($domain)
+    {
+        return $domain->getRules()->toArray();
+    }
+
+    /**
+     * get validation rules for preferences domains
+     */
+    protected function preferencesRules()
+    {
+        $rules = [];
+
+        $this->preferences
+            ->domains
+            ->map(function (Domain $domain) use (&$rules) {
+                $rules = array_merge($rules, $domain->getRules()->toArray());
+            });
+
+        return $rules;
+    }
+
+    /**
+     * @param \GeniusTS\Preferences\Models\Domain|null  $domain
+     * @param \GeniusTS\Preferences\Models\Element|null $element
+     *
      * @return Response
      */
-    abstract protected function handleSuccessResponse();
+    abstract protected function handleSuccessResponse($domain, $element);
 }
